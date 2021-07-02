eef49a759dcc7f6288a7cb27199f82cb0d40ab41
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2021-07-02 13:02:58 +0100

    Update netproc.py

--- netproc.py
+++ netproc.py
@@ -5,6 +5,6 @@ while True:
 	using_network=psutil.net_connections(kind='tcp')
 	rows=using_network[0:len(using_network)-1]
 	for i in range(0, len(rows)):
-		sconn=rows[i][6]
-		p = psutil.Process(sconn)
+		conn=rows[i][6]
+		p = psutil.Process(conn)
 		print ("Network most consuming process is: ",p.name())
